da6f7d747aec6c3ceba0bd997c55e4070c3173c7
Sokos6 <will@comicbooked.com>
2018-08-25 22:55:15 -0500

    stateless refactor

--- src/components/GymDayCount.js
+++ src/components/GymDayCount.js
@@ -1,32 +1,29 @@
-import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class GymDayCount extends Component {
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + "%")
-  }
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  }
-  render() {
-    return (
-      <div className="gym-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span>days</span>
-        </div>
-        <div className="cardio-days">
-          <span>{this.props.cardio}</span>
-          <span>days</span>
-        </div>
-        <div className="heavy-days">
-          <span>{this.props.heavy}</span>
-          <span>days</span>
-        </div>
-        <div>
-          <span>{this.calcGoalProgress(this.props.total, this.props.goal)}</span>
-        </div>
-      </div>
-    )
-  }
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total/goal)
+}
+
+export const GymDayCount = ({total, cardio, heavy, goal}) => (
+    <div className="gym-day-count">
+      <div className="total-days">
+        <span>{total}</span>
+        <span>days</span>
+      </div>
+      <div className="cardio-days">
+        <span>{cardio}</span>
+        <span>days</span>
+      </div>
+      <div className="heavy-days">
+        <span>{heavy}</span>
+        <span>days</span>
+      </div>
+      <div>
+        <span>{calcGoalProgress(total, goal)}</span>
+      </div>
+    </div>
+    )
