5b6e79438c6b4025a6ac50b8c5e31aaf47f9b5e8
Sokos6 <will@comicbooked.com>
2018-09-03 14:00:08 -0700

    Gym Day List

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { GymDayCount } from './components/GymDayCount'
+import { GymDayList } from './components/GymDayList'
 
 window.React = React
 
 render(
-  <GymDayCount total={50}
-    cardio={20}
-    heavy={10}
-    goal={100}/>,
+  <GymDayList = {
+    [
+      {
+        gym: "LA Fitness",
+        date: new Date("8/31/2018"),
+        cardio: true,
+        heavy: false
+      },
+      {
+        gym: "Anytime Fitness",
+        date: new Date("9/1/2018"),
+        cardio: true,
+        heavy: false
+      },
+      {
+        gym: "Onelife Fitness",
+        date: new Date("9/2/2018"),
+        cardio: true,
+        heavy: true   
+      }
+    ]
+  }/>,
+
+
   document.getElementById('react-container')
 )
